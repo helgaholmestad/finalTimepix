2cb0e95ad1d24ceea7a2c4d86a9be619b6fd5ff4
Helga Holmestad <helgamargrete@gmail.com>
2018-05-16 10:03:33 +0200

    adding files to do analysis for annihilations in different materials

---
+++ simu/vertexFitting/runParaCalium.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function run {
+    python GaussianBlurTCAD.py ../runningFLUKA/supersimpelTimepixCenterCalium$1_fort.22 $2 Calium &
+}
+
+run 001 1
+run 002 2
+run 003 3
+run 004 4
+run 005 5
+run 006 6
+run 007 7
+run 008 8
+run 009 9
+run 010 10
---
+++ simu/vertexFitting/runReClusterAl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function run {
+    python reClusterJustVolcano.py $1 "Al" &
+}
+
+run 1
+run 2
+run 3
+run 4
+run 5
+run 6
+run 7
+run 8
+run 9
+run 10
